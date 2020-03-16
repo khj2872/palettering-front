79d205661e9e28f41b6b6c67837b162977fe342f
bluewolf <uox0091@gmail.com>
2020-03-16 23:10:45 +0900

    [front-dev]vue 초기설정

---
+++ ui/src/components/main/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    홈피드화면
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: [],
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  destroyed() {},
+  mounted() {},
+  watch: {},
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ ui/src/views/main/MainView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from "@/components/main/Main.vue";
+
+export default {
+  name: "MainView",
+  props: [],
+  components: { Main },
+  data() {
+    return {};
+  },
+  created() {},
+  destroyed() {},
+  mounted() {},
+  watch: {},
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss"></style>
