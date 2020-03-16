c2ceb7a3b1c35cbf63d722c61241685b8978f69d
bluewolf <uox0091@gmail.com>
2020-03-16 22:48:26 +0900

    [front-dev]vue 초기설정

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,12 +1,39 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueRouter from 'vue-router'
 import store from "./store";
+
+import ElementUI from 'element-ui';
+
+//element-ui 언어 패키지
+import en from "element-ui/lib/locale/lang/en";
+import ko from "element-ui/lib/locale/lang/ko";
+
+import 'element-ui/lib/theme-chalk/index.css';
+
+let localLang = {};
+if (localStorage.getItem('lang') === 'en') {
+  localLang.locale = en;
+} else {
+  localLang.locale = ko;
+}
+
+
+Vue.use(ElementUI, localLang);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
+  el: '#app',
   store,
+  router,
   render: h => h(App)
-}).$mount("#app");
+});
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,30 +1,14 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import MainView from '@/views/MainView.vue'
 
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
+export const routes = [{
+    path: '/',
+    redirect: {
+      name: 'main'
+    }
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/main',
+    name: "main",
+    component: MainView
   }
-];
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
-
-export default router;
+]
